ee4dca5329ba31e0e86e379dbbd9b45af65bc5dc
Desarrollador1 <agustinschuler82@gmail.com>
2018-03-14 18:41:12 +0100

    xdlol

--- Practica_1/2.1.py
+++ Practica_1/2.1.py
@@ -13,20 +13,20 @@ rutaTraining = "training/frontal_"
 rutaTest = "testing/test"
 formato = ".jpg"
 #Training
-for i in range(48):
+for i in range(5):
     i = i + 1
     m = cv.imread(rutaTraining + str(i) + formato, 0)
     imagenes.append(m)
 #Keypoints
 orb = cv.ORB_create(nfeatures=300, nlevels=4, scaleFactor=1.3)
-# FLANN parameters
-FLANN_INDEX_LSH = 6
-index_params= dict(algorithm = FLANN_INDEX_LSH,
-                   table_number = 6, # 12
-                   key_size = 12,     # 20
-                   multi_probe_level = 1) #2
-search_params = dict(checks=50) # or pass empty dictionary
-flann = cv.FlannBasedMatcher(index_params, search_params)
+# # FLANN parameters
+# FLANN_INDEX_LSH = 6
+# index_params= dict(algorithm = FLANN_INDEX_LSH,
+#                    table_number = 6, # 12
+#                    key_size = 12,     # 20
+#                    multi_probe_level = 1) #2
+# search_params = dict(checks=50) # or pass empty dictionary
+# flann = cv.FlannBasedMatcher(index_params, search_params)
 
 distancesKP = []
 keyPoints = []
@@ -36,31 +36,18 @@ for imagen in imagenes:
 
     distancesKP.append(des)
     keyPoints.append(kps)
+# flann.add(distancesKP)
 
-for i in range(2):
-    imgTest = cv.imread(rutaTraining + "2" + formato, 0)
-    kpTest = orb.detect(imgTest, None)
-    kpsTest, desTest = orb.compute(imgTest, kpTest)
-    matches = flann.knnMatch(desTest, distancesKP[i], k=3)
-    # Need to draw only good matches, so create a mask
-    matchesMask = [[0,0] for i in range(len(matches))]
 
-    # ratio test as per Lowe's paper
-    # for i,(m,n) in enumerate(matches):
-    #     if m.distance < 0.7*n.distance:
-    #         matchesMask[i]=[1,0]
-    for match in matches:
-        if len(match) == 2:
-            if match[0].distance < 1 * match[1].distance:
-                matchesMask[i] = [1, 0]
-    draw_params = dict(matchColor = (0,255,0),
-                       singlePointColor = (255,0,0),
-                       matchesMask = matchesMask,
-                       flags = 0)
+imgTest = cv.imread(rutaTest + "2" + formato, 0)
+kpTest = orb.detect(imgTest, None)
+kpsTest, desTest = orb.compute(imgTest, kpTest)
 
-    img3 = cv.drawMatchesKnn(imgTest, kpTest, imagenes[i], keyPoints[i], matches, None, **draw_params)
-    cv.imshow("Resulta2", img3)
-    cv.waitKey()
+for i in range(5):
+    flann_params = dict(algorithm=1, trees=4)
+    flannIndex = cv.flann_Index(distancesKP[i], params=flann_params, distType=None)
+    idx, dist = flannIndex.knnSearch(desTest, 1, params={})
+
 
 
 
