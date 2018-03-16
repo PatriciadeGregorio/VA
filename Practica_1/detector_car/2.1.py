0589afdde93484944438b395f0a8b135a45ec3f2
Desarrollador1 <agustinschuler82@gmail.com>
2018-03-16 20:10:10 +0100

    Clase detector

---
+++ Practica_1/detector_car/2.1.py
@@ -0,0 +1,37 @@
+import cv2 as cv
+import numpy as np
+from detector_car.Detector import Detector
+imagenes = []
+rutaTraining = "../training/frontal_"
+formato = ".jpg"
+for i in range(48):
+    i = i + 1
+    m = cv.imread(rutaTraining + str(i) + formato, 0)
+    imagenes.append(m)
+
+    #Keypoints
+    orb = cv.ORB_create(nfeatures=500, nlevels=4)
+    # FLANN parameters
+    FLANN_INDEX_LSH = 6
+    index_params= dict(algorithm = FLANN_INDEX_LSH,
+                   table_number = 6, # 12
+                   key_size = 12,     # 20
+                  multi_probe_level = 1) #2
+    search_params = dict(checks=50) # or pass empty dictionary
+    flann = cv.FlannBasedMatcher(index_params, search_params)
+
+d = Detector(orb, flann, imagenes)
+flann = d.training(imagenes)
+for z in range(33):
+    z = z + 1;
+    imgTest = cv.imread("../testing/test" + str(z) + formato, 0)
+    pos_coche = d.test(imgTest=imgTest)
+    for x in range(imgTest.shape[0]):
+        for y in range(imgTest.shape[1]):
+            imgTest[pos_coche[0] * 10][y] = 0
+            imgTest[pos_coche[0] * 10 + 10][y] = 0
+            imgTest[x][pos_coche[1] * 10] = 0
+            imgTest[x][pos_coche[1] * 10 + 10] = 0
+
+    cv.imshow("", imgTest)
+    cv.waitKey()
---
+++ Practica_1/detector_car/Detector.py
@@ -0,0 +1,48 @@
+import cv2 as cv
+import numpy as np
+class Detector:
+    __orb = None
+    __flann = None
+    __imagenes = []
+    __DIVISION_MATRIZ = 10
+    descriptorsKP = []
+    keyPoints = []
+    def __init__(self, orb, flann, imagenes):
+        self.__orb = orb
+        self.__flann = flann
+        self.__imagenes = imagenes
+
+    def training(self, imagenes):
+        for imagen in imagenes:
+            kps = self.__orb.detect(imagen, None)
+            kps, des = self.__orb.compute(imagen, kps)
+
+            self.descriptorsKP.append(des)
+            self.keyPoints.append(kps)
+        self.__flann.add(self.descriptorsKP)
+
+    def test(self, imgTest):
+        # Testing
+        kpsTest = self.__orb.detect(imgTest, None)
+        kpsTest, desTest = self.__orb.compute(imgTest, kpsTest)
+        tuplaKD = zip(kpsTest, desTest)
+        matrizVotacion = np.zeros((np.int(imgTest.shape[0] / self.__DIVISION_MATRIZ), np.int(imgTest.shape[1] / self.__DIVISION_MATRIZ)), dtype=int)
+        for t in tuplaKD:
+            listaParecidos = self.__flann.knnMatch(t[1], k=5)
+            for parecido in listaParecidos:
+                for p in parecido:
+                    vector_votacion = self.__get_vector_votacion(self.keyPoints[p.imgIdx][p.trainIdx], t[0], p.imgIdx)
+                    if (vector_votacion[0] >= 0) & (vector_votacion[1] >= 0) & (
+                            vector_votacion[0] < (imgTest.shape[0] / self.__DIVISION_MATRIZ) - 1) & (
+                            vector_votacion[1] < (imgTest.shape[1] / self.__DIVISION_MATRIZ) - 1):
+                        matrizVotacion[vector_votacion[0], vector_votacion[1]] += 1
+        pos_coche = np.unravel_index(matrizVotacion.argmax(), matrizVotacion.shape)
+        return pos_coche
+
+    def __get_vector_votacion(self, kpParecido, kp, imgIdx):
+        img = self.__imagenes[imgIdx]
+        dimensiones = img.shape
+        centro = (dimensiones[0] / 2, dimensiones[1] / 2)
+        vector_v = (centro[0] - kpParecido.pt[0], centro[1] - kpParecido.pt[1])
+        return (np.int((kp.pt[0] + vector_v[0]) / self.__DIVISION_MATRIZ), np.int((kp.pt[1] + vector_v[1])/ self.__DIVISION_MATRIZ))
+
