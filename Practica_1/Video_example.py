c2874077736c47e0a78859d1e55b7cdcae9511ef
Desarrollador1 <agustinschuler82@gmail.com>
2018-03-27 19:55:30 +0200

    Codigo comentado. Se ha eliminado un fichero que no servia para nada.

--- Practica_1/Video_example.py
+++ Practica_1/Video_example.py
@@ -1,24 +1,30 @@
 import cv2 as cv
 from detector_car_2.Detector import DetectorCascade
 
+
+#Cargamos los videos
 cap = cv.VideoCapture('Videos/video1.wmv')
 
+#Cargamos el archivo para cargar el clasificador
 face_cascade_coche = cv.CascadeClassifier('haar/coches.xml')
 face_cascade_matricula = cv.CascadeClassifier('haar/matriculas.xml')
 
 while(True):
-    # Capture frame-by-frame
+
+    #Obtenemos un frame
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
+    #Lo pasamos a escalas de grises
     gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+    #Obtenemos las caras de las matriculas
     faces_matricula = face_cascade_matricula.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=6, minSize=(30, 30),
     flags=cv.CASCADE_SCALE_IMAGE)
     faces_coches = face_cascade_coche.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=6, minSize=(30, 30),
     flags=cv.CASCADE_SCALE_IMAGE)
+    #Usamos la clase DetectorCascade para dibujar las caras obtenidas
     dc = DetectorCascade(faces_coches, faces_matricula)
     dc.show_faces(gray)
-    # Display the resulting frame
+    # Mostramos el frame
     cv.imshow('frame',gray)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
--- Practica_1/detector_car_2/Ejercicio3.py
+++ Practica_1/detector_car_2/Ejercicio3.py
@@ -1,14 +1,16 @@
 import cv2 as cv
 from detector_car_2.Detector import DetectorCascade
 
-
+#Cargamos el archivo para cargar el clasificador
 face_cascade_coches = cv.CascadeClassifier('../haar/coches.xml')
 face_cascade_matriculas = cv.CascadeClassifier('../haar/matriculas.xml')
+#Se procede a cargar las imagenes de test para dibujar las caras en el
 rutaTesting = "../testing/test"
 formato = ".jpg"
 for i in range (33):
     i= i + 1
     img = cv.imread(rutaTesting + str(i) + formato, 0)
+    #Sacamos las caras correspondientes a los coches y a las matriculas
     facesCoches = face_cascade_coches.detectMultiScale(img, scaleFactor=1.05, minNeighbors=6, minSize=(30, 30),
     flags=cv.CASCADE_SCALE_IMAGE)
 
@@ -16,6 +18,7 @@ for i in range (33):
     facesMatricula = face_cascade_matriculas.detectMultiScale(img, scaleFactor=1.05, minNeighbors=6, minSize=(30, 30),
     flags=cv.CASCADE_SCALE_IMAGE)
 
+    #Cargamos el objeto para dibujar dichas caras y posteriormente se muestran
     dc = DetectorCascade(facesCoches, facesMatricula)
     dc.show_faces(img)
     cv.imshow('img', img)
